11a5ac2dd427897f4f9bbd5f9c2159c1b9a09b5a
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-21 13:46:51 -0500

    finished navbar and completely mobile friendly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
-import { FalinkedinIn} from 'react-icons/fa'
+import {AiOutlineMenu, AiOutlineClose, AiOutlineMail} from 'react-icons/ai'
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+            setNav(!nav)
+    }
+
     return (
     <div className='fixed w-full h-20 shadow-xl z-[100]' >
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -28,17 +35,20 @@ const Navbar = () => {
                     <li className=' ml-10 text-lg uppercase hover:border-b'>Contact</li>
                 </Link>
             </ul>
-            <div className='md:hidden'> 
+            <div onClick={handleNav} className='md:hidden'> 
                  <AiOutlineMenu size={25}/>       
             </div>
          </div>
       </div>
-        <div className='fixed left-0 top-0 w-full h-screen bg-black/60 '>
-            <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#86a2b8] p-10 ease-in duration-500'>
+
+        <div className={nav ? ' md:hidden fixed left-0 top-0 w-full h-screen bg-black/60' : '' }>
+            
+            <div className={nav ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#86a2b8] p-10 ease-in duration-500'
+                                : 'fixed left-[-100%]top-0 p-10 ease-in duration-500'}>
                 <div>
                 <div className= 'flex w-full items-center justify-between'>
-                    <Image scr='/../public/assets/projects/03e3aa07-4c87-4920-8e33-bb768cf58c53.jpg' width='87' height='35' alt='/'/>
-                    <div className='rounded-full shadow-lg shadow-gray-400 cursor-pointer'>
+                    <Image src='/../public/assets/projects/03e3aa07-4c87-4920-8e33-bb768cf58c53.jpg' width='60' height='35' alt='/'/>
+                    <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 cursor-pointer'>
                         <AiOutlineClose/>
                     </div>
                </div>
@@ -66,9 +76,18 @@ const Navbar = () => {
             </ul>
             <div className='pt-40 '>
             <p className='uppercase tracking-widest text-cyan-200'>Connect</p>
-            <div>
+            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                <FalinkedinIn/>
+                <FaLinkedinIn/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <FaGithub/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <AiOutlineMail/>
+                </div>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                <BsFillPersonLinesFill/>
                 </div>
             </div>
             </div>
