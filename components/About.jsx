9e27d2f9f60f917d8fcfa1bd9e4182c8f468209a
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-22 10:44:43 -0500

    stopped on 5226

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='col-span-2'></div>
+            <div></div>
+        </div>
+    </div>
   )
 }
 
