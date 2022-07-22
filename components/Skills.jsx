08db00f0bf024bb04c9d79f023e16a23fdde0569
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-22 15:45:32 -0500

    stopped 5822

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
+const Skills = () => {
+    return (
+        <div className='w-full lg:h-screen p-2'>
+            <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full' >
+            <p></p>
+            </div>
+        </div>
+    )
 }
+
+export default Skills 
