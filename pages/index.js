62a1ffd49f9272ca4723e0100bfc15c4e3282109
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-22 10:36:01 -0500

    created about comp and updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+
 
 
 export default function Home() {
@@ -14,6 +16,7 @@ export default function Home() {
       </Head>
     <Navbar />
     <Main/>
+    <About/>
       
     </div>
   )
