d8cbb990a7de611f1e2a2d515d391abc35acfeef
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-25 10:40:30 -0500

    added images to projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next-image';
+import propertyImg from '../public/assets/projects/books-beer-api.png'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className='w-full'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+            <p  className='text-xl tracking-widest uppercase text-black'>Projects</p>
+            <h2 className='py-4'>What I Have Built</h2>
+            <div className='grid md:grid-cols-2 gap-8'>
+
+            <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]' >
+                <Image src={propertyImg} alt='/'/>
+            </div>
+
+           </div>
+         </div>
+      </div>
+  );
+};
 
 export default Projects
